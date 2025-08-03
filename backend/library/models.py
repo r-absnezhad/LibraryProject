91e3d1e1cf4e74d5e64645849dd15e181d5632f5
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2025-08-03 22:49:57 +0330

    some settings and progresses
    + Static and media settings
    + Creating library app
    + Completing library app

---
+++ backend/library/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    """
+    This Model represents a book in the library.
+    Fields include title, author, ISBN, publication date, and a brief description.
+    """
+    pass
