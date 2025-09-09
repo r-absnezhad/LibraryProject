8671efadf07f088480c7d09bf2cbb05f254d319c
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2025-09-09 21:55:22 +0330

    solving some bugs

--- backend/loans/admin.py
+++ backend/loans/admin.py
@@ -3,9 +3,9 @@ from .models import Loan
 # Register your models here.
 class LoanAdmin(admin.ModelAdmin):
     model = Loan
-    list_display = ["id","profile","book", "borrowed_at", "due_date", "returned_at", "fine_amount"]
+    list_display = ["id","profile","book", "due_date", "returned_at", "fine_amount"]
     ordering = ["profile", "borrowed_at"]
     fieldsets = (
-        ("Details", {"fields": ("profile", "book", "borrowed_at", "due_date", "returned_at", "fine_amount")}),
+        ("Details", {"fields": ("profile", "book", "due_date", "returned_at", "fine_amount")}),
     )
 admin.site.register(Loan, LoanAdmin)
